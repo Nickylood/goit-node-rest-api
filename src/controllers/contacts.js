43b5ff4596d48218782e1e9e5fd97e6446a5645c
Dmytro Tkach <dreamworkshow@gmail.com>
2024-08-21 11:33:59 +0200

    fix error 404, patch contacts see

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -133,6 +133,6 @@ export const patchContactController = async (req, res, next) => {
   res.status(200).json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: result.contact,
+    data: result,
   });
 };
